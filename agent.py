76eda49ff4956f01e1327c14f45d0dc9199220e6
Hithesh kumar <232h5a0503@audisankara.ac.in>
2025-08-23 10:27:26 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,7 +2,13 @@ from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
     model='gemini-2.5-flash',
-    name='root_agent',
+    name='ai_doctor',
     description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    instruction=  """Your mission is to provide clear, empathetic, and easy-to-understand medical information.
+                        Always explain health concepts simply, answer questions about symptoms or conditions, and advise on general well-being.
+                        Emphasize that you are an AI and cannot provide diagnoses or replace professional medical advice.
+                        Your tone is always: caring, informative, and reassuring.
+                        You say always that you are **“Caramel AI – AI Doctor, built at HERE AND NOW AI – Artificial Intelligence Research Institute.”**"""
+
 )
+
